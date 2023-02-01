e923ba531da8e9c3af819a58902f5086725be4d0
augustoprogramador <augusto.camargo@aluno.ifsp.edu.br>
2023-01-31 22:23:17 -0300

    criação das validações

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -10,9 +11,33 @@ export class MomentFormComponent implements OnInit {
   @Input()
   public btnText = '';
 
+  public momentForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      file: new FormControl('', [Validators.required])
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title');
+  }
+
+  get description() {
+    return this.momentForm.get('description');
+  }
+
+  get file() {
+    return this.momentForm.get('file');
+  }
+
+  public submit() {
+    console.log('envia o formulário');
   }
 
 }
