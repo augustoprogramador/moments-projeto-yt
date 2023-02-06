508a782550c535ea6abec6dab9d86b93460d1b75
augustoprogramador <augusto.camargo@aluno.ifsp.edu.br>
2023-02-06 07:39:12 -0300

    inclusão do pacote @fortawesome, estilização do componente de mensageria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentsComponent } from './components/pages/new-moments/new-moments.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
-import { MessagesComponent } from './component/messages/messages.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { MessagesComponent } from './component/messages/messages.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
